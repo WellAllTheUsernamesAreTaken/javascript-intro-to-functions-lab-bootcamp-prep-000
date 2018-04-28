5c44e883bf6f573a7e14d0cf67626310e6e717ef
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-04-28 23:25:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
-    return "I can't hear you!"
+    return "I love you, too."
   } else if (sting === string.toUpperCase()) {
     return "YES INDEED!"
   } else if (string === string.toLowerCase()) {
-    return "I love you, too."
+    return "I can't hear you!"
   }
 }
