1aaaaa246d3729992a535c3c996fafb68da26b8a
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-04-28 23:30:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
 }
 function sayHiToGrandma(string) {
     if (string === string.toLowerCase()) {
-    return "I can't hear you!"
+    return "I can\'t hear you!"
   } 
 }
 
